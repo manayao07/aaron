a13af011783368310bdde1c2760a789ae4e49708
manayao07 <121504868+manayao07@users.noreply.github.com>
2022-12-27 23:43:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -122,7 +122,7 @@ until [[ "$opts" =~ ^[1]$ ]]; do
 
 	case $opts in
 	1)
-	until [[ "$SOCKSPORT" =~ ^[0-9]+$ ]] && [ "$SOCKSPORT" -ge 1 ] && [ "$SOCKSPORT" -le 55555 ]; do "55555" SOCKSPORT
+	until [[ "$SOCKSPORT" =~ ^[0-9]+$ ]] && [ "$SOCKSPORT" -ge 1 ] && [ "$SOCKSPORT" -le 55555 ];
 	done
 	SOCKSAUTH='none'
 	Installation
